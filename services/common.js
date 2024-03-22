ba62f2341797aff4998e05edb9ff2b993937bacd
Sarim Khan <sarim.khan0505@gmail.com>
2024-03-22 16:57:48 +0530

    Backend Part 6 : Connected Auth with React with Cookie

--- services/common.js
+++ services/common.js
@@ -1,9 +1,19 @@
 const passport = require('passport');
 
 exports.isAuth = (req, res, done) => {
-  return passport.authenticate('jwt')
+  return passport.authenticate('jwt');
 };
 
 exports.sanitizeUser = (user) => {
-  return { id: user.id, role: user.role }
-}
+  return { id: user.id, role: user.role };
+};
+
+exports.cookieExtractor = function (req) {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies['jwt'];
+  }
+  //TODO : this is temporary token for testing without cookie
+  token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZmQ2NTc2Y2EwN2E2NjcyM2RhNjM5MSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTcxMTEwNjEyMX0.C4jY0LmTmZse-DJuFrO-CXCg8VUgMFPfn3u9Gk0nWx8"
+  return token;
+};
